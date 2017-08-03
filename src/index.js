64350a6f555b613f40744140af30f0edf15cf7d5
Andrew Joslin <andrewtjoslin@gmail.com>
2017-08-03 09:39:31 -0600

    Init

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
-import './style';
-import App from './components/app';
+import {Component} from 'preact'
+import Router from 'preact-router'
 
-export default App;
+import Home from './routes/home'
+
+export default class App extends Component {
+  render () {
+    return <div id="app">
+      App Content
+      <Router>
+        <Home path='/' />
+      </Router>
+    </div>
+  }
+}
--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,13 +1,3 @@
-import { h, Component } from 'preact';
-import style from './style';
-
-export default class Home extends Component {
-	render() {
-		return (
-			<div class={style.home}>
-				<h1>Home</h1>
-				<p>This is the Home component.</p>
-			</div>
-		);
-	}
+export default function Home () {
+  return <div>Home Content</div>
 }
